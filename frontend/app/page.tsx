3483bbc8f02abb990f4adcfbe5b50676f26bcd8e
Ishaan Dey <ishaan1013@gmail.com>
2023-11-18 15:35:21 -0500

    add color theme

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen items-start justify-center overflow-hidden pt-32">
       <nav className="flex w-full max-w-screen-lg flex-col items-center justify-start px-4">
-        <h1 className="max-w-screen-md text-center text-4xl font-semibold">
-          Alzheimer&apos;s Disease &amp; Monitoring
+        <h1 className="text-center text-4xl font-semibold">
+          Alzheimer&apos;s Disease{" "}
+          <span className="text-blue-600">Detection &amp; Monitoring</span>
         </h1>
       </nav>
     </main>
