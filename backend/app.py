60a89fae883477c2e00928510f79ef5e3316c151
KrishM123 <kahnscatterkrish@gmail.com>
2023-11-19 06:25:32 -0500

    semantic search

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
 import openai
+import os
 
 api = Flask(__name__)
 
@@ -26,6 +27,24 @@ def gpt_accuracy(answer1, answer2):
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": f"Determine the accuracy of the given answer to the real answer. Accuracy should be on a scale of 1 to 10 with 10 being almost same. Only return the number. Given answer:{answer1} True answer: {answer2}"}
+            ]
+        )
+    return response['choices'][0]['message']['content']
+
+@api.route('/search')
+def search():
+    query = request.args.get('query')
+    
+    ref = {}
+    for folder1 in os.listdir(os.getcwd() + "\\summary\\"):
+        for file in os.listdir(os.getcwd() + "\\summary\\" + folder1):
+            ref["\\summary\\" + folder1 + '\\' + file] = open(file, "r").read()
+
+    response = openai.ChatCompletion.create(
+            engine='gpt-4',
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"In which file is the answer to the following question contained: File and content dictionary {ref}. Question: {query}"}
             ]
         )
     return response['choices'][0]['message']['content']
