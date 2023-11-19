fbe6565546244a735189f4f87438c3311067a42a
Ishaan Dey <ishaan1013@gmail.com>
2023-11-19 06:55:14 -0500

    working local flask app

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,12 @@ openai.api_key = 'APIKEY2'
 openai.api_base = 'https://api.umgpt.umich.edu/azure-openai-api/ptu'
 openai.api_version = '2023-03-15-preview'
 
+
 @api.route('/questions')
 def questions():
     with open("qas.json", "r") as file:
         return json.load(file)
+
 
 @api.route('/accuracy')
 def accuracy():
@@ -21,30 +23,38 @@ def accuracy():
     real = request.args.get('real')
     return gpt_accuracy(given, real)
 
+
 def gpt_accuracy(answer1, answer2):
     response = openai.ChatCompletion.create(
-            engine='gpt-4',
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"Determine the accuracy of the given answer to the real answer. Accuracy should be on a scale of 1 to 10 with 10 being almost same. Only return the number. Given answer:{answer1} True answer: {answer2}"}
-            ]
-        )
+        engine='gpt-4',
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"Determine the accuracy of the given answer to the real answer. Accuracy should be on a scale of 1 to 10 with 10 being almost same. Only return the number. Given answer:{answer1} True answer: {answer2}"}
+        ]
+    )
     return response['choices'][0]['message']['content']
+
 
 @api.route('/search')
 def search():
     query = request.args.get('query')
-    
+
     ref = {}
-    for folder1 in os.listdir(os.getcwd() + "\\summary\\"):
-        for file in os.listdir(os.getcwd() + "\\summary\\" + folder1):
-            ref["\\summary\\" + folder1 + '\\' + file] = open(file, "r").read()
+
+    # works on mac; change path for windows
+    userpath = os.path.expanduser('~/Documents/Programming/mhacks/backend')
+
+    for folder1 in os.listdir(userpath+"/summary/"):
+        for file in os.listdir(userpath+"/summary/" + folder1):
+            ref[userpath+"/video_chunks/" + folder1 + '_chunks/' +
+                file.split(".")[0] + '.mp4'] = open(userpath+"/summary/" + folder1 + '/' + file, "r", encoding="utf8", errors="ignore").read()
 
     response = openai.ChatCompletion.create(
-            engine='gpt-4',
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"In which file is the answer to the following question contained: File and content dictionary {ref}. Question: {query}"}
-            ]
-        )
+        engine='gpt-4',
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user",
+                "content": f"In which file is the answer to the following question contained (return only the file path(s)): File and content dictionary {ref}. Question: {query}"}
+        ]
+    )
     return response['choices'][0]['message']['content']
